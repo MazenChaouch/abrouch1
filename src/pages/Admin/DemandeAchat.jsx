94abcea8d370b92a68c67bccf886f8af61c71709
sickmanmazen <m.chaouch007@gmail.com>
2022-12-09 00:21:51 +0100

    ploy

--- src/pages/Admin/DemandeAchat.jsx
+++ src/pages/Admin/DemandeAchat.jsx
@@ -1,34 +1,72 @@
-import { useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { collection, onSnapshot, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { Button, Container, Form, Table } from "react-bootstrap";
+import { fireStore } from "../../auth/Firebase";
 import NavBarAdmin from "../../componants/NavBarAdmin"
 
 const DemandeAchat = () => {
     const [idd, setIdd] = useState();
     const [date, setDate] = useState();
+    const [fourniseur , setFourniseur] = useState([]);
+    const [fourniseurs , setFourniseurs] = useState([]);
+    const getFourniseurs = async () => {
+        const q = query(collection(fireStore, "fourniseur"));
+        onSnapshot(q, (querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                const result = querySnapshot.docs
+                    .map((doc) => ({ ...doc.data(), id: doc.id }));
+                setFourniseurs(result);
+            });
+        });
+    }
+    useEffect(() => {
+        getFourniseurs();
+    }, [])
     return (
         <>
             <NavBarAdmin />
             <div className="bg">
                 <div className="d-flex justify-content-center align-items-center h-100">
-                    <div className="glass-effect responsiv p-5">
-                        <h1 className="fw-bolder fs-1 text-center">Demande d'achat</h1>
-                        <Form className="mt-4 ">
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Id</Form.Label>
-                                <Form.Control type="text" value={idd} onChange={(e) => setIdd(e.target.value)} className="form-control" required placeholder="Enter email" />
-
-                            </Form.Group>
-
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Date</Form.Label>
-                                <Form.Control type="date" value={date} onChange={(e) => setDate(e.target.value)} className="form-control" required placeholder="Password" />
-                            </Form.Group>
-                            <div className="d-grid gap-2">
-                                <Button variant="outline-dark" type="submit" >
-                                    Login
-                                </Button>
-                            </div>
-                        </Form>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <Container>
+                                <Table striped bordered hover size="sm" variant="dark">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>fourniseur</th>
+                                            <th>produit</th>
+                                            <th>quantité</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>{
+                                    }
+                                    </tbody>
+                                </Table>
+                            </Container>
+                        </div>
+                        <div className="glass-effect responsive p-5 col-md-6">
+                            <h1 className="fw-bolder fs-1 text-center">ajout de produit</h1>
+                            <Form className="mt-4 ">
+                                <Form.Select aria-label="Default select example" value={fourniseur} onChange={(e) => setFourniseur(e.currentTarget.value)}>
+                                    <option>selectionner un fourniseur</option>
+                                    {
+                                        fourniseurs.map((f, index) => {
+                                            return <option tabIndex={index} value={f.nom}>{f.nom}</option>
+                                        })
+                                    }
+                                </Form.Select>
+                                <Form.Group className="mb-3" controlId="formBasicPassword">
+                                    <Form.Label>Date</Form.Label>
+                                    <Form.Control type="date" value={date} onChange={(e) => setDate(e.target.value)} className="form-control" required placeholder="Password" />
+                                </Form.Group>
+                                <div className="d-grid gap-2">
+                                    <Button variant="outline-dark" type="submit" >
+                                        ajouter
+                                    </Button>
+                                </div>
+                            </Form>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/pages/Admin/Produit.jsx
+++ src/pages/Admin/Produit.jsx
@@ -1,101 +1,130 @@
 import { collection, onSnapshot, query, setDoc, doc, deleteDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { Button, Container, Table } from "react-bootstrap";
+import { Button, Container, Form, Table } from "react-bootstrap";
 import { fireStore } from "../../auth/Firebase";
 import NavBarAdmin from "../../componants/NavBarAdmin"
 import generateId from "../../lib/generatedId";
 
 const Produit = () => {
-    const [produit, setProduit] = useState([]);
+    const [produits, setProduits] = useState([]);
+    const [fourniseur, setFourniseur] = useState([]);
     const [nom, setNom] = useState("");
-    const [idp, setIdp] = useState("");
     const [quantity, setQuantity] = useState("");
     const [prix, setPrix] = useState("");
 
-    let id = generateId(10)
-    const addProduit = (e) => {
-        e.preventDefault();
+    
+    const addProduit = () => {
+        let id = generateId(4)
         setDoc(doc(fireStore, "produit", id), {
-            idp: idp,
+            fourniseur: fourniseur,
             nom: nom,
             quantity: quantity,
             prix: prix
         })
     };
 
-
-    const getProduit = async () => {
+    
+    const [fourniseurs, setFourniseurs] = useState([]);
+    const getFourniseurs = async () => {
+        const q = query(collection(fireStore, "fourniseur"));
+        onSnapshot(q, (querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                const result = querySnapshot.docs
+                    .map((doc) => ({ ...doc.data(), id: doc.id }));
+                setFourniseurs(result);
+            });
+        });
+    }
+    
+    const getProduits = async () => {
         const q = query(collection(fireStore, "produit"));
         onSnapshot(q, (querySnapshot) => {
             querySnapshot.forEach((doc) => {
                 const result = querySnapshot.docs
                     .map((doc) => ({ ...doc.data(), id: doc.id }));
-                setProduit(result);
+                setProduits(result);
             });
         });
     }
     useEffect(() => {
-        getProduit();
+        getFourniseurs();
+        getProduits();
     }, [])
     return (
         <>
-            <NavBarAdmin />
-            <div className="bg">
-                <Container>
-                    <div className="fs-1 fw-bold text-center p-5">
-                        Les Produits
-                    </div>
-                    <Table className="table-bordered">
-                        <thead>
-                            <tr className="text-center bg-dark text-white">
-                                <th>#</th>
-                                <th>Id</th>
-                                <th>Nom</th>
-                                <th>Quantité</th>
-                                <th>Prix</th>
-                                <th>supprimer</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                produit.map((p, index) => {
-                                    return (
-                                        <tr key={index} className="text-center bg-light text-black" >
-                                            <td>{index + 1}</td>
-                                            <td>{p.idp}</td>
-                                            <td>{p.nom}</td>
-                                            <td>{p.quantity}</td>
-                                            <td>{p.prix}</td>
-                                            <td><Button variant="danger" size="sm" onClick={deleteDoc(doc(fireStore, "produit", p.id))}>X</Button></td>
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
-                    </Table>
-                    <form className="row g-3 mt-2 rounded" onSubmit={addProduit}>
-                        <div className="col-md-3">
-                            <label for="inputEmail4" className="form-label fw-bold">Id</label>
-                            <input type="text" value={idp} onChange={(e) => setIdp(e.currentTarget.value)} className="form-control" placeholder="Entrer le nom de la formation" required />
+            <NavBarAdmin /><div className="bg">
+                <div className="d-flex justify-content-center align-items-center h-100">
+                    <Container className="row">
+                        <div className="col-md-6">
+
+                            <Table striped bordered hover size="sm" variant="dark">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Fourniseur</th>
+                                        <th>Nom</th>
+                                        <th>Quantité</th>
+                                        <th>Prix</th>
+                                        <th>Supprimer</th>
+                                    </tr>
+                                </thead>
+                                <tbody>{
+                                    produits.map((p, index) => {
+                                        return (
+                                            <tr key={index} className="bg-light text-black" >
+                                                <td>{index + 1}</td>
+                                                <td>{p.fourniseur}</td>
+                                                <td>{p.nom}</td>
+                                                <td>{p.quantity}</td>
+                                                <td>{p.prix}</td>
+                                                <td><Button variant="danger" size="sm" onClick={() => deleteDoc(doc(fireStore, "produit", p.id))}>X</Button></td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                                </tbody>
+                            </Table>
                         </div>
-                        <div className="col-md-3">
-                            <label for="inputEmail4" className="form-label fw-bold">Nom</label>
-                            <input type="text" value={nom} onChange={(e) => setNom(e.currentTarget.value)} className="form-control" placeholder="Entrer le nom de la formation" required />
+                        <div className="glass-effect col-sm-6">
+
+                            <h1 className="fw-bolder fs-1 text-center">ajout de produit</h1>
+                            <Form className="mt-4 row" onSubmit={addProduit}>
+                                <Form.Group className="mb-3 col-sm-6" controlId="formBasicPassword">
+                                    <Form.Label>Fourniseur</Form.Label>
+                                    <Form.Select aria-label="Default select example" value={fourniseur} onChange={(e) => setFourniseur(e.currentTarget.value)}>
+                                        <option>selectionner un fourniseur</option>
+                                        {
+                                            fourniseurs.map((f, index) => {
+                                                return <option tabIndex={index} value={f.nom}>{f.nom}</option>
+                                            })
+                                        }
+
+                                    </Form.Select>
+                                </Form.Group>
+                                <Form.Group className="mb-3 col-sm-6" controlId="formBasicPassword">
+                                    <Form.Label>nom</Form.Label>
+                                    <Form.Control type="text" value={nom} onChange={(e) => setNom(e.target.value)} className="form-control" required placeholder="entre le nom de produit" />
+                                </Form.Group>
+                                <Form.Group className="mb-3 col-sm-6" controlId="formBasicPassword">
+                                    <Form.Label>Quantité</Form.Label>
+                                    <Form.Control type="text" value={quantity} onChange={(e) => setQuantity(e.target.value)} className="form-control" required placeholder="entre la quantité de produit" />
+                                </Form.Group>
+                                <Form.Group className="mb-3 col-sm-6" controlId="formBasicPassword">
+                                    <Form.Label>Prix</Form.Label>
+                                    <Form.Control type="text" value={prix} onChange={(e) => setPrix(e.target.value)} className="form-control" required placeholder="entre le prix de produit" />
+                                </Form.Group>
+                                <div className="d-grid gap-2">
+                                    <Button variant="outline-dark" type="" >
+                                        ajouter
+                                    </Button>
+                                </div>
+                            </Form>
+
                         </div>
-                        <div className="col-md-3">
-                            <label for="inputEmail4" className="form-label fw-bold">Quantité</label>
-                            <input type="text" value={quantity} onChange={(e) => setQuantity(e.currentTarget.value)} className="form-control" placeholder="Entrer le nom de la formation" required />
-                        </div>
-                        <div className="col-md-3">
-                            <label for="inputPassword4" className="form-label fw-bold">Prix</label>
-                            <input type="text" value={prix} onChange={(e) => setPrix(e.currentTarget.value)} className="form-control" placeholder="Entrer le nom du formateur" required />
-                        </div>
-                        <div className="col-12">
-                            <button type="submit" className="btn btn-primary">Ajouter</button>
-                        </div>
-                    </form>
-                </Container>
+                    </Container>
+                </div>
             </div>
+
         </>
     )
 }
